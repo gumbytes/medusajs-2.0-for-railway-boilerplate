b10df16e0284452e85638f5c0341dd792b31cf03
shamelzik <121865281+shamelzik@users.noreply.github.com>
2024-09-17 01:03:22 +0800

    +

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -30,7 +30,22 @@ const modules = {
     options: { 
       redisUrl: process.env.REDIS_URL
     }
-  }
+  },
+  [Modules.NOTIFICATION]: {
+    resolve: "@medusajs/notification",
+    options: {
+      providers: [
+        {
+          resolve: "./modules/resend-notificatio",
+          id: "my-notification",
+          options: {
+            channels: ["email"],
+            apiKey: process.env.RESEND_API_KEY
+          },
+        },
+      ],
+    },
+  },
 };
 
 // Stripe payment provider
@@ -38,7 +53,7 @@ const stripeApiKey = process.env.STRIPE_API_KEY;
 const stripeWebhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 const stripeConfigured = stripeApiKey && stripeWebhookSecret;
 if (stripeConfigured) {
-  console.log('Stripe api key and webhook secret found, enabling stripe payment provider');
+  console.log('Stripe API key and webhook secret found, enabling Stripe payment provider');
   modules[Modules.PAYMENT] = {
     resolve: '@medusajs/payment',
     options: {
@@ -61,7 +76,7 @@ const sendgridApiKey = process.env.SENDGRID_API_KEY;
 const sendgridFrom = process.env.SENDGRID_FROM_EMAIL;
 const sendgridConfigured = sendgridApiKey && sendgridFrom;
 if (sendgridConfigured) {
-  console.log('SendGrid api key and from address found, enabling SendGrid notification provider');
+  console.log('SendGrid API key and from address found, enabling SendGrid notification provider');
   modules[Modules.NOTIFICATION] = {
     resolve: '@medusajs/notification',
     options: {
